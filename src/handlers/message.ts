91945337a4160da3e148d9f412ceb0d006d99d0c
matthew <stead873@gmail.com>
2020-12-12 22:27:21 +0000

    🔊 Add heartbeat packet logging

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -4,5 +4,7 @@ export default function handleMessage(message: string, flag: any, websocket: any
         return websocket.emit("ready", msg.d.user);
     } else if (msg.t === "MESSAGE_CREATE") {
         return websocket.emit("message", msg.d);
+    } else if (msg.op == 10) {
+        return console.log('You\'ll need to ack the packet ' + JSON.stringify(msg.d))
     }
 }
