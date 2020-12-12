ad39bcb9fa87705d4e7b83965af9f19a0ddb4748
Dice <qbotdev84@gmail.com>
2020-12-12 22:49:39 +0000

    There was a trailing }

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -3,7 +3,7 @@ import { green, bold } from "chalk";
 export default function handleMessage(message: string, flag: any, websocket: any) {
     const msg = websocket.evaluate(message, flag);
     if (msg.t === "READY") {
-        websocket.emit("debug", `${green.bold("[NOTICE/websocket]")} ${green("Connected to the Discord API")}}`)
+        websocket.emit("debug", `${green.bold("[NOTICE/websocket]")} ${green("Connected to the Discord API")}`)
         return websocket.emit("ready", msg.d.user);
     } else if (msg.t === "MESSAGE_CREATE") {
         return websocket.emit("message", msg.d);
