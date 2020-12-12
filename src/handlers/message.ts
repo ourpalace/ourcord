6ae3ed56f7ad8c22f64b5bf22617e153c3d05093
matthew <stead873@gmail.com>
2020-12-12 23:03:15 +0000

    🐛 Fix heartbeat interval not clearing

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -5,6 +5,10 @@ export default function handleMessage(message: string, flag: any, websocket: any
     } else if (msg.t === "MESSAGE_CREATE") {
         return websocket.emit("message", msg.d);
     } else if (msg.op == 10) {
+        if (websocket.hb) clearInterval(websocket.hb)
+        websocket.hb = setInterval(() => {
+            websocket.emit('debug', `[Heartbeat] - ${msg.d.heartbeat_interval}ms`)
+        }, msg.d.heartbeat_interval)
         return console.log(`You'll need to ack the packet ${JSON.stringify(msg.d)}`)
     }
 }
