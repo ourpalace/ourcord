c02630539b377276daf41fa7ca8a11b855d92c5a
Dice <qbotdev84@gmail.com>
2020-12-12 16:33:08 +0000

    Expand index.ts

--- src/example.index.ts
+++ src/example.index.ts
@@ -1,3 +1,12 @@
 import Client from "./websocket";
 
-const client = new Client();
+const client = new Client(/* YOUR TOKEN HERE */);
+
+client.on("ready", () => {
+    console.log("Client connected to discord API")
+});
+
+client.on("message", async msg => {
+    const tested = await client.sendMessage(msg.channel_id, "yes");
+    console.log(tested);
+});
