0ebc84d5cf5b22083e58b6ec30aaffbae98f9d4f
Misly <60405462+Misly16@users.noreply.github.com>
2020-12-13 10:29:17 +0000

    proposal of new eslint configs and styling guide

--- examples/ts/client.ts
+++ examples/ts/client.ts
@@ -1,10 +1,10 @@
-import Client from "ourcord";
+import Client from 'ourcord';
 
-const client = new Client("Your token here", {
-    prefix: "!"
+const client = new Client('Your token here', {
+  prefix: '!',
 }); // client options here
 client.connect();
 
-client.on("ready", () => {
-    console.log("Client ready");
+client.on('ready', () => {
+  console.log('Client ready');
 });
