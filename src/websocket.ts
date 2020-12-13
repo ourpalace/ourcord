4a795908801c4ff8361d5d927b014c0a3142f3be
VoltrexMaster <62040526+VoltrexMaster@users.noreply.github.com>
2020-12-13 23:52:52 +0330

    Cleanups, documentation improvements, better implementations
    Cleaned up redundant code, added new and missing and improved documentations and JSDoc and improved method implantations.

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,16 +1,16 @@
 // initial imports
 import ws from 'ws';
 import fetch from 'node-fetch';
-import {EventEmitter as Emitter} from 'events';
-import {red, yellow, bold} from 'chalk';
+import { EventEmitter as Emitter } from 'events';
+import { red, yellow, bold } from 'chalk';
 import os from 'os';
 import pako from 'pako';
 // @ts-ignore
 import zlib from 'fast-zlib';
-import {config} from 'dotenv';
+import { config } from 'dotenv';
 import handlers from './handlers/handlers.index';
-import {statusTypesArray, authHeader} from './utils';
-import {Cache} from './caches/base';
+import { statusTypesArray, authHeader } from './utils';
+import { Cache } from './caches/base';
 // import { connect } from "./client_functions";
 
 config();
@@ -37,22 +37,20 @@ export interface ClientOptions {
 	browser?: string;
 	device?: string;
 	prefix?: string;
-	cacheChannels?: boolean,
-    cacheGuilds?: boolean,
-    cacheUsers?: boolean,
-    cacheMembers?: boolean,
-	activity?: {name: string, type: number};
-	status?: 'dnd' | 'invisible' | 'online' | 'idle';
+      cacheChannels?: boolean,
+      cacheGuilds?: boolean,
+      cacheUsers?: boolean,
+      cacheMembers?: boolean,
+	activity?: { name: string, type: number };
+	status?: 'online' | 'idle' | 'dnd' | 'invisible';
 }
 
-export interface StatusInfo {
-
-}
+export interface StatusInfo {}
 
 /**
- * @param {string} token the token used to login to the gateway
- * @param {any} socket
- * @param {any} config
+ * @param {string} token The client's token used for gateway connection.
+ * @param {any} socket The socket.
+ * @param {any} config The configurations.
  */
 export class Client extends Emitter {
 	token: string;
@@ -61,115 +59,127 @@ export class Client extends Emitter {
 	hb: any;
 	config: ClientOptions;
 	cache: any;
+
 	/**
-	 *
-	 * @param {string} token the token used to login to the gateway
-	 * @param {ClientOptions} options ClientOptions
+	 * The main client constructor.
+	 * @param {string} token The client's token used for gateway connection.
+	 * @param {ClientOptions} options Options this client is instantiated with.
 	 */
 	constructor(token: string, options?: ClientOptions) {
 	  super();
-	  if (!token) throw new Error(`${red.bold('[ERROR/websocket]')} ${red('No token was provided')}`);
-	  this.token = token;
-	  if (!options) {
-	    this.config = {
+	  if (!token) throw new Error(`${red.bold('[ERROR/Websocket]')} ${red('Expected a client token')}`);
+          // Using 'Object.defineProperty()' to prevent the token from being enumerable.
+	  Object.defineProperty(this, 'token', {
+             value: token,
+             writable: true,
+             enumerable: false
+          });
+	  this.config = options || {
 	    browser: 'ourcord (https://github.com/ourcord/ourcord)',
 	    device: 'ourcord (https://github.com/ourcord/ourcord)',
-	    status: 'dnd',
+	    status: 'dnd'
 	  };
-	  } else this.config = options;
 	  this.cache = new Cache(this, this.config);
 	}
 
-	connect() {
-	  this.emit('debug', `${yellow.bold('[NOTICE/websocket]')} ${yellow('Attempting to connect to the discord gateway')}`);
+        /**
+         * The method used to connect to the gateway.
+         * @returns {undefined}
+         */
+	connect(): void {
+	  this.emit('debug', `${yellow.bold('[NOTICE/Websocket]')} ${yellow('Attempting to connect to the discord gateway')}`);
 	  // eslint-disable-next-line new-cap
 	  this.socket = new ws('wss://gateway.discord.gg/?v=6&encoding=json');
 	  this.socket.once('open', () => {
-	    this.emit('debug', `${yellow.bold('[NOTICE/websocket]')} ${yellow('Attempting to login')}`);
+	    this.emit('debug', `${yellow.bold('[NOTICE/Websocket]')} ${yellow('Attempting to login')}`);
 	    const data = JSON.stringify(this.getMetaData());
 	    this.socket.send(data);
-	    this.socket.once('error', (error: string) => {
-	      handlers.errorHandler(error, this);
-	    });
-	    this.socket.on('message', (message: any, flag: any) => {
-	      handlers.messageHandler(message, flag, this);
-	    });
+	    this.socket.once('error', (error: string) => handlers.errorHandler(error, this));
+	    this.socket.on('message', (message: any, flag: any) => handlers.messageHandler(message, flag, this));
 	    this.socket.on('close', (h: any) => {
-		  clearInterval(this.hb);
-		  this.emit('debug', `${bold('[NOTICE/websocket]')} Connection closed unexpectedly (code ${h}). Re-attempting login`);
+	      clearInterval(this.hb);
+	      this.emit('debug', `${bold('[NOTICE/Websocket]')} ${red(`Connection closed unexpectedly (code ${h}). Re-attempting login`)}`);
 	      this.connect();
 	    });
 	  });
 	};
 
 	/**
- *
- * @param {string} reason the reason the socket was closed
- */
-	destroy(reason?: string) {
+         * The method used to destroy the client and close the connection to the websocket.
+         * @param {string} [reason] The reason to close the socket.
+         * @returns {undefined}
+         */
+	destroy(reason?: string): void {
 	  this.socket.close();
-	  this.emit('debug', `${red.bold('[NOTICE/websocket]')} ${red(reason ? reason : 'The websocket was closed')}`);
+	  this.emit('debug', `${red.bold('[NOTICE/Websocket]')} ${red(reason || 'The websocket was closed')}`);
 	};
+
 	/**
- *
- * @param {string} channel the channel ID which the message will be sent in
- * @param {any} content the body of the message
- */
-	async _sendMessage(channel: string, content: string | object) {
+         * The method used to send a message to a TextChannel.
+         * @param {string} channel ID of the TextChannel the message will be sent in.
+         * @param {(string|object)} content The body of the message.
+         * @returns {Promise<object>}
+         */
+	async _sendMessage(channel: string, content: string | object): Promise<object> {
 	  const url = `https://discord.com/api/v7/channels/${channel}/messages`;
 	  let b: MessageProperties = {};
-	  if (!content || !content.toString().length) throw new Error('[ERROR/discordAPI error] Cannot send a message with no content');
+	  if (content === null || typeof content === 'undefined' || !content.toString().length) throw new Error(`${red.bold('[ERROR/DiscordAPI Error]')} Cannot send a message with no content`);
 	  if (typeof content === 'string') b.content = content;
 	  if (typeof content === 'object') b = content;
-	  const sent = await fetch(url, {
+	  return await fetch(url, {
 	    method: 'POST',
 	    headers: {
 	      'Authorization': authHeader(this.token),
-	      'Content-Type': 'application/json',
+	      'Content-Type': 'application/json'
 	    },
-	    body: JSON.stringify(b),
-	  });
-	  return await sent.json();
+	    body: JSON.stringify(b)
+	  }).then(res => res.json());
 	};
+
 	/**
- *
- * @param {string} channel the channel ID which the message will be sent in
- * @param {any} options the properties of the embed
- */
-	async _MessageEmbed(channel: string, options: EmbedProperties) {
+         * The method used to send an embed in a TextChannel.
+         * @param {string} channel ID of the TextChannel the message will be sent in.
+         * @param {EmbedProperties} options The embed data.
+         * @returns {Promise<object>}
+         */
+	async _MessageEmbed(channel: string, options: EmbedProperties): Promise<object> {
 	  const url = `https://discord.com/api/v7/channels/${channel}/messages`;
-	  if (!options) throw new Error('[ERROR/discordAPI error] Cannot send a message with no content');
-	  const data = await fetch(url, {
+	  if (options === null || typeof options === 'undefined') throw new Error(`${red.bold('[ERROR/DiscordAPI Error]')} Cannot send a message with no content`);
+	  return await fetch(url, {
 	    method: 'POST',
 	    headers: {
 	      'Authorization': authHeader(this.token),
-	      'Content-Type': 'application/json',
+	      'Content-Type': 'application/json'
 	    },
-	    body: JSON.stringify(options),
-	  });
-	  return await data.json();
+	    body: JSON.stringify(options)
+	  }).then(res => res.json());
 	};
+
 	/**
-	 *
-	 * @param {string} userID the userID to get from Discord's rest API
+	 * The method used to fetch a user from the rest discord API.
+	 * @param {string} userID The ID of the user to fetch.
+       * @returns {Promise<object>}
 	 */
-	async _GetRestUser(userID: string) {
+	async _GetRestUser(userID: string): Promise<object> {
 	  const url = `https://discord.com/api/v7/users/${userID}`;
-	  if (!userID || !userID.toString().length) throw new Error('[ERROR/discordAPI error] Please provide a userID');
-	  const data = await fetch(url, {
+	  if (userID === null || typeof userID === 'undefined' || !userID.toString().length) throw new Error(`${red.bold('[ERROR/DiscordAPI Error]')} ${userID} is not snowflake`);
+	  return await fetch(url, {
 	    method: 'GET',
 	    headers: {
 	      'Authorization': authHeader(this.token),
-	      'Content-Type': 'application/json',
-	    },
-	  });
-	  return await data.json();
+	      'Content-Type': 'application/json'
+	    }
+	  }).then(res => res.json());
 	};
 
+        /**
+         * The method used to get the metadata.
+         * @returns {object}
+         */
 	getMetaData(): object {
 	  const metaData = {
-	    op: 2,			// opcode of 2 means "identify"
-	    d: {			// d is for data
+	    op: 2, // opcode of 2 means "identify"
+	    d: { // d is for data
 	      token: this.token,
 	      properties: {
 	        $os: os.platform,
@@ -178,56 +188,63 @@ export class Client extends Emitter {
 	      },
 	      presence: {
 	        // activities: [{name: this.config.activity.name ? this.config.activity.name : null, type: 0}],
-	        status: this.config.status,
-	      },
-	    },
+	        status: this.config.status
+	      }
+	    }
 	  };
 	  return metaData;
 	};
+
 	/**
- *
- * @param {any} data
- * @param {any} flag
- * @return {string}
- */
+         * Evaluates under the hood stuff.
+         * @param {any} data The data to evaluate.
+         * @param {any} flag The flags for evaluation.
+         * @return {string}
+         */
 	evaluate(data: any, flag: any) {
 	  if (typeof flag !== 'object') flag = {};
-	  if (!flag.binary) return JSON.parse(data);
+	  if (flag.binary === null || typeof flag.binary === 'undefined') return JSON.parse(data);
 	  const inflateData = new pako.Inflate();
 	  inflateData.push(data);
-	  if (inflateData.err) throw new Error('[ERROR/pako error] An error occured while decompressing data');
+	  if (inflateData.err) throw new Error(`${red.bold('[ERROR/Pako Error]')} An error occured while decompressing data`);
 	  return JSON.parse(inflateData.toString());
 	};
 
-	// eslint-disable-next-line require-jsdoc
-	setStatus(t: 'dnd' | 'invisible' | 'online' | 'idle') {
-	  if (!statusTypesArray.includes(t)) {
-	    throw new Error('[ERROR/discordAPI error] Status provided is incorrect');
-	  }
+	/**
+       * The method used to set the status of the client.
+       * @param {('online'|'idle'|'dnd'|'invisible')} t The status type to set client's status to.
+       * @returns {undefined}
+       */
+	setStatus(t: 'online' | 'idle' | 'dnd' | 'invisible'): void {
+	  if (!statusTypesArray.includes(t)) throw new Error(`${red.bold('[ERROR/DiscordAPI Error]')} Invalid status type`);
 	  try {
 	    const p = JSON.stringify({
 	      op: 3,
 	      d: {
-			  status: t,
-			  afk: false,
-			  since: t == 'idle' ? Date.now() : null,
-			  game: null,
+               status: t,
+	         afk: false,
+	         since: t === 'idle' ? Date.now() : null,
+	         game: null,
 	      },
-		  });
+            });
 	    console.log(p);
 	    this.socket.send(p);
 	  } catch (err) {
 	    throw new Error(err);
 	  }
-	}
+	};
+
 	/**
-	 *
-	 * @param {string} g the guild ID in which the channel should be created in
-	 * @param {string} name the name of the channel to be created
+	 * The method used to create a GuildChannel
+	 * @param {string} g ID of the guild where the channel will be created in.
+	 * @param {string} name The name of the channel
+         * @returns {Promise<object>}
 	 */
-	async createChannel(g: string, name: string) {
+	async createChannel(g: string, name: string): Promise<object> {
+          if (typeof g !== 'string') throw new Error(`${red.bold('[ERROR/DiscordAPI Error]')} ${g} is not snowflake`);
+          if (typeof name !== 'string') throw new Error(`${red.bold('[ERROR/DiscordAPI Error]')} The channel name is required`);
 	  const url = `https://discord.com/api/v7/guilds/${g}/channels`;
-	  const channel = await fetch(url, {
+	  return await fetch(url, {
 	    method: 'POST',
 	    headers: {
 	      'Content-Type': 'application/json',
@@ -236,9 +253,8 @@ export class Client extends Emitter {
 	    body: JSON.stringify({
 	      name,
 	    }),
-	  }).then((c) => c.json());
-	  return channel;
-	}
+	  });
+	};
 }
 
 export default Client;
