f170e69748676ba5fbe3ebd0cbd02912433f0d31
Misly <60405462+Misly16@users.noreply.github.com>
2020-12-13 12:05:54 +0000

    jsdoc

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,10 @@
 export const statusTypesArray = ['online', 'dnd', 'idle', 'invisible'];
 export const activityTypesArray = ['COMPETING', 'LISTENING', 'STREAMING', 'PLAYING'];
+/**
+ *
+ * @param {string} token the clients token
+ * @return {string} the auth header used by Discord
+ */
 export function authHeader(token: string) {
   return `Bot ${token}`;
 }
