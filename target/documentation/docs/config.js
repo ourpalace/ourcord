8b2a3d6a6acd54074660fe7b66bcf2f90adb7c78
65502847 <qbotdev84@gmail.com>
2020-12-15 18:50:44 +0000

    removed an oopsie dir

--- target/documentation/docs/config.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.fileTree = [{ "name": "handlers", "children": [] }, { "name": "utils", "path": "/utils", "fullPath": "./documentation/docs/utils" }, { "name": "websocket", "path": "/websocket", "fullPath": "./documentation/docs/websocket" }];
-exports.sidebarTree = (title = 'Mainpage') => ({ "/docs/": [{ "title": "ourcord", "collapsable": false, "children": [["", "" + title + ""], "utils", "websocket"] }] });
