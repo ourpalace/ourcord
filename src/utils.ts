66d352b14204fb1b41f13e6fc27616d7eeaacc8e
65502847 <qbotdev84@gmail.com>
2020-12-18 15:18:02 +0000

    added native slash commands (requires the application.commands intent) and linted other files

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,4 @@
 export const statusTypesArray = ['online', 'idle', 'dnd', 'invisible'];
 export const activityTypesArray = ['PLAYING', 'LISTENING', 'WATCHING', 'STREAMING', 'COMPETING'];
 export const apiBaseURL = "https://discord.com/api/v7";
+export const v8BaseURl = "https://discord.com/api/v8";
