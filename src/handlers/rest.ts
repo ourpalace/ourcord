9a9e94a7a8303305cd335ccceff7f982cb504580
AndrewDaGuy <66189242+AndrewDaGuy@users.noreply.github.com>
2020-12-20 15:18:26 -0800

    Update rest.ts

--- src/handlers/rest.ts
+++ src/handlers/rest.ts
@@ -1,9 +1,6 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable object-curly-spacing*/
 import { Rest } from '../structures/rest';
 import User from "../structures/user";
 
-// eslint-disable-next-line require-jsdoc
 export default function restClient(id: string) {
 
 }
