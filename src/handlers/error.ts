4987a2d33ad1eec46815a67333aaaa6e0748bd9b
AndrewDaGuy <66189242+AndrewDaGuy@users.noreply.github.com>
2020-12-20 15:15:40 -0800

    Yeeted out eslint

--- src/handlers/error.ts
+++ src/handlers/error.ts
@@ -1,7 +1,6 @@
-/* eslint-disable object-curly-spacing*/
 import { EventEmitter } from 'events';
 
-// eslint-disable-next-line require-jsdoc
+
 export default function handleErr(err: string, emitter: EventEmitter): boolean {
   return emitter.emit('error', err);
 }
