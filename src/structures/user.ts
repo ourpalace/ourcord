240032f840792ac2a74658c09eae9082e0cca04e
null <60427892+vierofernando@users.noreply.github.com>
2020-12-22 09:37:31 +0700

    Update user.ts

--- src/structures/user.ts
+++ src/structures/user.ts
@@ -6,9 +6,22 @@ export default class User {
     discriminator: string;
     avatar: string;
     verified: boolean;
+    bot: boolean;
     email: string;
     flags: number;
     premium_type: number;
     public_flags: number;
-    constructor(r: object) {}
+    constructor(r: object) {
+        this.id = object.id;
+        this.username = object.username;
+        this.name = object.username; // alias for this.username;
+        this.discriminator = object.discriminator;
+        this.avatar = object.avatar;
+        this.verified = object.verified;
+        this.email = object.email;
+        this.bot = object.bot;
+        this.flags = object.flags;
+        this.premium_type = object.premium_type;
+        this.public_flags = object.public_flags;
+    }
 }
