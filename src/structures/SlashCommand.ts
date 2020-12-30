5db4c6f93d1903e6207f200a11142020f90065c9
VoltrexMaster <62040526+VoltrexMaster@users.noreply.github.com>
2020-12-30 11:05:49 +0330

    Cleanups, improvements and refactors
    1- Cleaned up redundant code
    2- Improved some properties
    3- Refactoring of many checks

--- src/structures/SlashCommand.ts
+++ src/structures/SlashCommand.ts
@@ -1,31 +1,28 @@
 /* eslint-disable */
-import * as utils from "../utils";
-import fetch from "node-fetch";
-import Client from "../websocket";
-import { red, bold } from "chalk";
+import * as utils from '../utils';
+import fetch from 'node-fetch';
+import Client from '../websocket';
+import { red, bold } from 'chalk';
+
+declare interface Choice {
+   name: string;
+   value: string;
+}
 
 export interface OptionsProps {
   name: string;
   description: string;
-  typeID:
-  | "SUB_COMMAND"
-  | "SUB_COMMAND_GROUP"
-  | "STRING"
-  | "INTEGER"
-  | "BOOLEAN"
-  | "USER"
-  | "CHANNEL"
-  | "ROLE";
+  typeID: 'SUB_COMMAND' | 'SUB_COMMAND_GROUP' | 'STRING' | 'INTEGER' | 'BOOLEAN' | 'USER' | 'CHANNEL' | 'ROLE';
   type?: number;
   required?: boolean;
-  choices?: Array<{ name: string; value: string }>;
+  choices?: Choice[];
   default?: boolean;
 }
 
 export interface SlashConfig {
   name: string;
   description: string;
-  options: Array<OptionsProps>;
+  options: OptionsProps[];
 };
 
 const CommandTypes = {
@@ -41,37 +38,30 @@ const CommandTypes = {
 
 export default class SlashCommand {
   constructor(client: Client, config: SlashConfig) {
-    const h = CommandTypes["SUB_COMMAND"]
-    const postUrl = utils.v8BaseURl + `/applications/${client.user.id}/commands`;
-    if (/ /g.test(config.name) || config.options.some(opt => / /g.test(opt.name))) {
-      throw new Error(`${bold.red("[ERROR/DISCORDAPI Error]")} ${red("Slash command names can't have spaces")}`);
-    } else if (!config) {
-      throw new Error(`${bold.red("[ERROR/DISCORDAPI Error]")} ${red("You need to pass in a config argument")}`);
-    } else if (!config.options || !config.name || !config.description) {
-      throw new Error(`${bold.red("[ERROR/DISCORDAPI Error]")} ${red("Missing argument")}`);
-    } else if (!config.options.every(conf => typeof conf.name !== "undefined") || !config.options.every(conf => typeof conf.description !== "undefined") || !config.options.every(conf => typeof conf.typeID !== "undefined")) {
-      throw new Error(`${bold.red("[ERROR/DISCORDAPI Error]")} ${red("Missing argument")}`);
-    } else if (config.options.filter(o => o.default).length > 1) {
-      throw new Error(`${bold.red("[ERROR/DISCORDAPI Error]")} ${red("There can only be one default option")}`);
-    } else if (config.options.length > 10) {
-      throw new Error(`${bold.red("[ERROR/DISCORDAPI Error]")} ${red("Maximum of 10 options per slash command")}`);
-    } else if (!config.options.some(opt => typeof opt.typeID !== "number") && !config.options.some(opt => CommandTypes[opt.typeID])) {
-      throw new Error(`${bold.red("[ERROR/DISCORDAPI Error]")} ${red("Invalid command option type")}`)
-    }
-    getSlash(client, postUrl, config);
+    const h = CommandTypes['SUB_COMMAND'];
+    const postUrl = `${utils.v8BaseURl}/applications/${client.user.id}/commands`;
+    if (!config) throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('\'config\' argument is required')}`);
+    if (typeof config.name !== 'string') throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('\'config.name\' must be a string')}`);
+    if (typeof config.description !== 'string') throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('\'config.description\' must be a string')}`);
+    if (!Array.isArray(config.options)) throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('\'config.options\' must be an array')}`);
+    if (config.options.length > 10) throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('Cannot have more than 10 options in \'config.options\'')}`);
+    if (config.options.filter(({ default }) => default).length > 1) throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('There can only be 1 default command option')}`);
+    let properties = ['typeID', 'name', 'description'];
+    if (!properties.slice(1).some(property => property in config) || !properties.some(property => config.options.every(c => typeof c[property] !== 'undefined'))) throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('Missing arguments in \'config\'')}`);
+    if (!config.options.some(({ typeID }) => typeof typeID === 'number') || !config.options.some(({ typeID }) => CommandTypes[typeID])) throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('Invalid command option type')}`);
+    if (config.name.includes(' ') || config.options.some(({ name }) => name.includes(' '))) throw new Error(`${bold.red('[ERROR/DiscordAPIError]')} ${red('Command option name cannot include spaces')}`);
+    return getSlash(client, postUrl, config);
   }
 }
 
 async function getSlash(client: Client, postUrl: string, config: SlashConfig): Promise<SlashCommand> {
-  config.options.forEach(opt => opt.type = CommandTypes[opt.typeID])
-  const body = JSON.stringify(config)
-  let res = await fetch(postUrl, {
-    method: "POST",
+  for (let option of config.options) option.type = CommandTypes[option.typeID];
+  return await fetch(postUrl, {
+    method: 'POST',
     headers: {
-      'Authorization': `Bot ${client.token}`,
+      Authorization: `Bot ${client.token}`,
       'Content-Type': 'application/json'
     },
-    body: body
-  });
-  return res.json();
+    body: JSON.stringify(config)
+  }).then(res => res.json());
 }
